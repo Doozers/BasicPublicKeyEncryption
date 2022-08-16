4e8defcd67be5b10c895460db134eb1ed9949ef2
ismael FALL <ismael.fall@epitech.eu>
2022-08-16 15:58:13 +0200

    :dizzy:

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Doozers/BasicPublicKeyEncryption/lib"
+	"os"
+)
+
+func main() {
+	err := lib.GenKeys("private1.key", "public1.key")
+	if err != nil {
+		return
+	}
+
+	privKey, err := os.ReadFile("public1.key")
+	if err != nil {
+		return
+	}
+	pubKey, err := os.ReadFile("private1.key")
+	if err != nil {
+		return
+	}
+
+	crypt := lib.Sign((*[64]byte)(privKey), []byte("Hello World"))
+	fmt.Println(crypt)
+	fmt.Println(lib.Verify((*[32]byte)(pubKey), crypt))
+}
